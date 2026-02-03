679945471ec4a18b5aaca0fd7e2842bf48ec0cc3
Adithya Firmansyah Putra <aditputrafirmansyah@gmail.com>
2026-02-03 09:50:34 +0700

    feat(01-02): implement LocalDataSource
    - Wrap SwiftData with ModelContext
    - getUser, saveUser, updateUser methods
    - Uses FetchDescriptor for queries
    - Throws errors per PROJECT_RULES.md

--- surahfocus/Sources/Data/DataSource/LocalDataSource.swift
+++ surahfocus/Sources/Data/DataSource/LocalDataSource.swift
@@ -6,10 +6,37 @@
 //
 
 import Foundation
+import SwiftData
 
-// Local data source stub - will be populated in future phases
 final class LocalDataSource {
-    static let shared = LocalDataSource()
+    let container: ModelContainer
+    let context: ModelContext
 
-    private init() {}
+    init(container: ModelContainer) {
+        self.container = container
+        self.context = ModelContext(container)
+    }
+
+    func getUser() throws -> User? {
+        let descriptor = FetchDescriptor<User>()
+        var users = try context.fetch(descriptor)
+        return users.first
+    }
+
+    func saveUser(_ user: User) throws {
+        context.insert(user)
+        try context.save()
+    }
+
+    func updateUser(_ apply: (User) -> Void) throws {
+        guard let user = try getUser() else {
+            throw LocalDataSourceError.userNotFound
+        }
+        apply(user)
+        try context.save()
+    }
+}
+
+enum LocalDataSourceError: Error {
+    case userNotFound
 }
